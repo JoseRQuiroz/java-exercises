f802d6ab5651793cd9837d97287c2ae6c7d05584
Jose Quiroz <JoseRQuiroz@gmail.com>
2018-12-13 15:13:21 -0600

    initial commit

--- src/org/launchcode/java/demos/java4python/methods/Message.java
+++ src/org/launchcode/java/demos/java4python/methods/Message.java
@@ -4,12 +4,12 @@ package org.launchcode.java.demos.java4python.methods;
  * Created by LaunchCode
  */
 public class Message {
+// getMessage returns a message in a given language
+    public static String getMessage(String language) {
 
-    public static String getMessage(String lang) {
-
-        if (lang.equals("sp")) {
+        if (language.equals("sp")) {
             return "Hola Mundo";
-        } else if (lang.equals("fr")) {
+        } else if (language.equals("fr")) {
             return "Bonjour le monde";
         } else {
             return "Hello World";
